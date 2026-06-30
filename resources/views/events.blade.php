7728a1760cf0b2c0e780812fa2dfb631e96e59df
IZMAdt <izmadts@gmail.com>
2026-06-30 06:45:27 +0500

    fix event.blade.php

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -296,8 +296,8 @@
 
 
      <!-- JavaScript Libraries -->
-     <script src=" {{ asset('https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-     <script src=" {{ asset('https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+     <script src=" https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+     <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
      <script src=" {{ asset('lib/wow/wow.min.js') }}"></script>
      <script src=" {{ asset('lib/easing/easing.min.js') }}"></script>
      <script src=" {{ asset('lib/waypoints/waypoints.min.js') }}"></script>
